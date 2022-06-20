6b972cedb69cbf6a80f81580659690fbb6cecd8e
Alexander Trujillo <atrujilloochoa@gmail.com>
2022-06-20 17:14:30 +0200

    adding missing extra tests

--- labs/lab-01/tests.cc
+++ labs/lab-01/tests.cc
@@ -26,3 +26,23 @@ TEST(Pythagoras, ScalarProduct)
   ASSERT_EQ(x * x, 25);
 }
 
+TEST(Pythagoras, Norm3D)
+{
+  Point<3> x(1, 2, 2);
+  ASSERT_EQ(x.norm(), 3);
+}
+
+
+TEST(Pythagoras, Distance3D)
+{
+  Point<3> x(5, 1, 1);
+  Point<3> y(1, 3, 5);
+  ASSERT_EQ(x.distance(y), 6);
+}
+
+
+TEST(Pythagoras, ScalarProduct3D)
+{
+  Point<3> x(3, 4, 5);
+  ASSERT_EQ(x * x, 50);
+}
