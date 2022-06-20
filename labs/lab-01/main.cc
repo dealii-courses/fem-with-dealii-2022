5244f9a7d1ca056e9e78739e0fefecc2c9521939
Alexander Trujillo <atrujilloochoa@gmail.com>
2022-06-20 17:04:48 +0200

    uploading lab-01 sol

---
+++ labs/lab-01/main.cc
@@ -0,0 +1,13 @@
+#include <deal.II/base/point.h>
+
+#include <gtest/gtest.h>
+
+using namespace dealii;
+
+int
+main(int argc, char *argv[])
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
