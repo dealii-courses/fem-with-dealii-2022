0ef82eed93106b793e84b4f4256e093113223f61
Luca Heltai <luca.heltai@sissa.it>
2022-06-21 22:12:45 +0200

    Fixed include.

--- labs/lab-04/source/main.cc
+++ labs/lab-04/source/main.cc
@@ -1,10 +1,10 @@
-#include "step-3.h"
+#include "poisson.h"
 
 int
 main()
 {
   deallog.depth_console(2);
-  Step3 laplace_problem;
+  Poisson laplace_problem;
   laplace_problem.run();
   return 0;
 }
